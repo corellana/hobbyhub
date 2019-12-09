20b42458ba5f96f745aaa0928b6a3989ed445d2f
Carito <carito.orellana@gmail.com>
2019-12-08 19:43:53 -0800

    Starting Point Project

---
+++ Models/User.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+namespace Project.Models
+{
+    public class User
+    {
+        // auto-implemented properties need to match the columns in your table
+        // the [Key] attribute is used to mark the Model property being used for your table's Primary Key
+        [Key]
+        public int Id { get; set; }
+        // MySQL VARCHAR and TEXT types can be represeted by a string
+
+        // First Name ------------------------------------------------------------
+        [Required(ErrorMessage = "You must enter a First Name")]
+        [MinLength(2, ErrorMessage = "First Name must be at least 2 characters")]
+        public string FirstName { get; set; }
+
+        // Last Name ------------------------------------------------------------
+        [Required(ErrorMessage = "You must enter a Last Name")]
+        [MinLength(2, ErrorMessage = "Last Name must be at least 2 characters")]
+        public string LastName { get; set; }
+
+        // Email ------------------------------------------------------------
+        [Required(ErrorMessage = "You must enter an email")]
+        // [MinLength(2, ErrorMessage = "Name must be at least 2 characters")]
+        public string Email { get; set; }
+
+        // Password ------------------------------------------------------------
+        [Required(ErrorMessage = "You must enter an password")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        public string Password { get; set; }
+
+        // Create and Update  ------------------------------------------------------------
+        // The MySQL DATETIME type can be represented by a DateTime
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Will not be mapped to your users table!
+        [NotMapped]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirmation { get; set; }
+    }
+}
